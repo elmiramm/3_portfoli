7b84edb8d3dbf333d30c989a16bc7932af7047e9
elmiramm <elmira.m.m@yandex.ru>
2022-11-13 13:58:48 +0300

    jqwery, animation

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ $(function () {
 	let modalClose = $("[data-close]");
 
 	modalCall.on("click", function (event) {
-
 		event.preventDefault();
 
 		let $this = $(this);
@@ -41,25 +40,43 @@ $(function () {
 
 		$(modalId).addClass('show');
 		$("body").addClass('no-scroll');
+
+		setTimeout(function () {
+			$(modalId).find(".modal__dialog").css({ transform: "rotateX(0)" })
+		}, 200);
+
+
 	})
 
 	modalClose.on("click", function (event) {
-
 		event.preventDefault();
 
 		let $this = $(this);
 		let modalParent = $this.parents('.modal');
 
-		modalParent.removeClass('show');
-		$("body").removeClass('no-scroll');
+		$(modalParent).find(".modal__dialog").css({
+			transform: "rotateX(90deg)"
+		})
+
+		setTimeout(function () {
+			modalParent.removeClass('show');
+			$("body").removeClass('no-scroll');
+		}, 200)
+
+
 	})
 
 	$(".modal").on("click", function (event) {
+		let $this = $(this);
+		$this.find(".modal__dialog").css({
+			transform: "rotateX(90deg)"
+		})
 
-		event.preventDefault();
+		setTimeout(function () {
+			$this.removeClass('show');
+			$("body").removeClass('no-scroll');
+		}, 200)
 
-		$(this).removeClass('show');
-		$("body").removeClass('no-scroll');
 	})
 
 	$(".modal__dialog").on("click", function (event) {
