b608620cf6e4afaf7fa96f4733eb86c3d574c325
elmiramm <elmira.m.m@yandex.ru>
2022-11-13 13:40:03 +0300

    jqwery. modal

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(function () {
 
+
+	// Filter
 	let filter = $("[data-filter]");
 
 	filter.on("click", function (event) {
@@ -22,12 +24,47 @@ $(function () {
 
 			})
 		}
-
-
-
-
 	})
 
+
+	// Modal
+
+	let modalCall = $("[data-modal");
+	let modalClose = $("[data-close]");
+
+	modalCall.on("click", function (event) {
+
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalId = $this.data('modal');
+
+		$(modalId).addClass('show');
+		$("body").addClass('no-scroll');
+	})
+
+	modalClose.on("click", function (event) {
+
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalParent = $this.parents('.modal');
+
+		modalParent.removeClass('show');
+		$("body").removeClass('no-scroll');
+	})
+
+	$(".modal").on("click", function (event) {
+
+		event.preventDefault();
+
+		$(this).removeClass('show');
+		$("body").removeClass('no-scroll');
+	})
+
+	$(".modal__dialog").on("click", function (event) {
+		event.stopPropagation();
+	})
 
 
 })
